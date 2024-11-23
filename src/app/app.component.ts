75d28fa0a0902a27a0fd7e8debad3f70c0f6fc2a
Sunnat Amirov <sunnatbekamirov22@gmail.com>
2024-11-23 12:56:27 +0500

    cv-project created with angular. Initial commit.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { IntroductionComponent } from './introduction/introduction.component';
+import { EducationComponent } from './education/education.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { SkillsComponent } from './skills/skills.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IntroductionComponent, EducationComponent, ExperienceComponent, SkillsComponent, ProjectsComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
